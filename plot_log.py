f395ec1d56454d0a7d9e811cb289d9dbb084c4a5
dkasuga <benjamin.at.mac@gmail.com>
2020-07-17 16:47:55 +0900

    check TODO

--- plot_log.py
+++ plot_log.py
@@ -17,6 +17,7 @@ def running_mean(x, N):
 
 def load_logs(experiment_directory, type):
 
+    # TODO:
     logs = torch.load(os.path.join(experiment_directory, ws.logs_filename))
 
     logging.info("latest epoch is {}".format(logs["epoch"]))
@@ -64,7 +65,8 @@ def load_logs(experiment_directory, type):
 
     elif type == "lat_mag":
         ax.plot(logs["latent_magnitude"])
-        ax.set(xlabel="Epoch", ylabel="Magnitude", title="Latent Vector Magnitude")
+        ax.set(xlabel="Epoch", ylabel="Magnitude",
+               title="Latent Vector Magnitude")
 
     elif type == "param_mag":
         for _name, mags in logs["param_magnitude"].items():
@@ -83,7 +85,8 @@ if __name__ == "__main__":
 
     import argparse
 
-    arg_parser = argparse.ArgumentParser(description="Plot DeepSDF training logs")
+    arg_parser = argparse.ArgumentParser(
+        description="Plot DeepSDF training logs")
     arg_parser.add_argument(
         "--experiment",
         "-e",
