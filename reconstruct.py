385fed7b8f6f9d4141a0031ffa2651d6ae82e70a
dkasuga <benjamin.at.mac@gmail.com>
2020-07-17 16:48:20 +0900

    rewrite from torch to TF

--- reconstruct.py
+++ reconstruct.py
@@ -12,6 +12,9 @@ import torch
 import deep_sdf
 import deep_sdf.workspace as ws
 
+import numpy as np
+import tensorflow as tf
+
 
 def reconstruct(
     decoder,
@@ -27,7 +30,8 @@ def reconstruct(
     def adjust_learning_rate(
         initial_lr, optimizer, num_iterations, decreased_by, adjust_lr_every
     ):
-        lr = initial_lr * ((1 / decreased_by) ** (num_iterations // adjust_lr_every))
+        lr = initial_lr * ((1 / decreased_by) **
+                           (num_iterations // adjust_lr_every))
         for param_group in optimizer.param_groups:
             param_group["lr"] = lr
 
@@ -35,55 +39,57 @@ def reconstruct(
     adjust_lr_every = int(num_iterations / 2)
 
     if type(stat) == type(0.1):
-        latent = torch.ones(1, latent_size).normal_(mean=0, std=stat).cuda()
+        latent = tf.random.normal([1, latent_size], mean=0, std=stat)
     else:
-        latent = torch.normal(stat[0].detach(), stat[1].detach()).cuda()
+        latent = tf.convert_to_tensor(np.random.normal(stat[0], stat[1]))
 
-    latent.requires_grad = True
-
-    optimizer = torch.optim.Adam([latent], lr=lr)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 
     loss_num = 0
-    loss_l1 = torch.nn.L1Loss()
 
-    for e in range(num_iterations):
+    def loss_l1(y_pred, y_true):
+        return tf.math.reduce_sum((y_pred - y_true))
 
-        decoder.eval()
+
+   for e in range(num_iterations):
+
         sdf_data = deep_sdf.data.unpack_sdf_samples_from_ram(
             test_sdf, num_samples
-        ).cuda()
+        )
         xyz = sdf_data[:, 0:3]
-        sdf_gt = sdf_data[:, 3].unsqueeze(1)
+        sdf_gt = tf.expand_dims(sdf_data[:, 3], axis=1)
 
-        sdf_gt = torch.clamp(sdf_gt, -clamp_dist, clamp_dist)
+        sdf_gt = tf.clip_by_value(sdf_gt, -clamp_dist, clamp_dist)
 
         adjust_learning_rate(lr, optimizer, e, decreased_by, adjust_lr_every)
 
-        optimizer.zero_grad()
 
-        latent_inputs = latent.expand(num_samples, -1)
+        with tf.GradientTape() as tape:
+            latent_inputs = tf.broadcast_to(latent, [num_samples, -1])
 
-        inputs = torch.cat([latent_inputs, xyz], 1).cuda()
+            inputs = tf.concat([latent_inputs, xyz], axis=1)
 
-        pred_sdf = decoder(inputs)
+            pred_sdf = decoder(inputs, training=False)
 
-        # TODO: why is this needed?
-        if e == 0:
-            pred_sdf = decoder(inputs)
+            # TODO: why is this needed?
+            if e == 0:
+                pred_sdf = decoder(inputs, training=False)
 
-        pred_sdf = torch.clamp(pred_sdf, -clamp_dist, clamp_dist)
+            pred_sdf = tf.clip_by_value(pred_sdf, -clamp_dist, clamp_dist)
 
-        loss = loss_l1(pred_sdf, sdf_gt)
-        if l2reg:
-            loss += 1e-4 * torch.mean(latent.pow(2))
-        loss.backward()
-        optimizer.step()
+            loss = loss_l1(pred_sdf, sdf_gt)
+            if l2reg:
+                loss += 1e-4 * tf.reduce_mean(tf.math.pow(latent, 2))
+
+        grad_latent = tape.gradient(loss, latent)
+
+        optimizer.apply_gradients(zip(grad_latent, latent))
 
         if e % 50 == 0:
-            logging.debug(loss.cpu().data.numpy())
+            logging.debug(loss.numpy())
             logging.debug(e)
-            logging.debug(latent.norm())
-        loss_num = loss.cpu().data.numpy()
+            logging.debug(tf.norm(latent))
+        loss_num = loss.numpy()
 
     return loss_num, latent
 
@@ -143,12 +149,13 @@ if __name__ == "__main__":
     deep_sdf.configure_logging(args)
 
     def empirical_stat(latent_vecs, indices):
-        lat_mat = torch.zeros(0).cuda()
+        lat_mat = tf.zeros(0)
         for ind in indices:
-            lat_mat = torch.cat([lat_mat, latent_vecs[ind]], 0)
-        mean = torch.mean(lat_mat, 0)
-        var = torch.var(lat_mat, 0)
+            lat_mat = tf.concat([lat_mat, latent_vecs[ind]], axis=0)
+        mean = tf.math.reduce_mean(lat_mat, axis=0)
+        var = tf.math.square(tf.math.reduce_std(lat_mat, axis=0))
         return mean, var
+
 
     specs_filename = os.path.join(args.experiment_directory, "specs.json")
 
@@ -165,8 +172,10 @@ if __name__ == "__main__":
 
     decoder = arch.Decoder(latent_size, **specs["NetworkSpecs"])
 
+    # TODO:
     decoder = torch.nn.DataParallel(decoder)
 
+    # TODO:
     saved_model_state = torch.load(
         os.path.join(
             args.experiment_directory, ws.model_params_subdir, args.checkpoint + ".pth"
@@ -174,14 +183,16 @@ if __name__ == "__main__":
     )
     saved_model_epoch = saved_model_state["epoch"]
 
+    # TODO:
     decoder.load_state_dict(saved_model_state["model_state_dict"])
 
-    decoder = decoder.module.cuda()
+    # decoder = decoder.module.cuda()
 
     with open(args.split_filename, "r") as f:
         split = json.load(f)
 
-    npz_filenames = deep_sdf.data.get_instance_filenames(args.data_source, split)
+    npz_filenames = deep_sdf.data.get_instance_filenames(
+        args.data_source, split)
 
     random.shuffle(npz_filenames)
 
@@ -193,7 +204,8 @@ if __name__ == "__main__":
     rerun = 0
 
     reconstruction_dir = os.path.join(
-        args.experiment_directory, ws.reconstructions_subdir, str(saved_model_epoch)
+        args.experiment_directory, ws.reconstructions_subdir, str(
+            saved_model_epoch)
     )
 
     if not os.path.isdir(reconstruction_dir):
@@ -216,7 +228,8 @@ if __name__ == "__main__":
         if "npz" not in npz:
             continue
 
-        full_filename = os.path.join(args.data_source, ws.sdf_samples_subdir, npz)
+        full_filename = os.path.join(
+            args.data_source, ws.sdf_samples_subdir, npz)
 
         logging.debug("loading {}".format(npz))
 
@@ -229,10 +242,12 @@ if __name__ == "__main__":
                     reconstruction_meshes_dir, npz[:-4] + "-" + str(k + rerun)
                 )
                 latent_filename = os.path.join(
-                    reconstruction_codes_dir, npz[:-4] + "-" + str(k + rerun) + ".pth"
+                    reconstruction_codes_dir, npz[:-4] +
+                    "-" + str(k + rerun) + ".pth"
                 )
             else:
-                mesh_filename = os.path.join(reconstruction_meshes_dir, npz[:-4])
+                mesh_filename = os.path.join(
+                    reconstruction_meshes_dir, npz[:-4])
                 latent_filename = os.path.join(
                     reconstruction_codes_dir, npz[:-4] + ".pth"
                 )
@@ -246,8 +261,8 @@ if __name__ == "__main__":
 
             logging.info("reconstructing {}".format(npz))
 
-            data_sdf[0] = data_sdf[0][torch.randperm(data_sdf[0].shape[0])]
-            data_sdf[1] = data_sdf[1][torch.randperm(data_sdf[1].shape[0])]
+            data_sdf[0] = data_sdf[0][tf.random.shuffle(tf.range(start=0, limit=data_sdf[0].shape[0]))]
+            data_sdf[1] = data_sdf[1][tf.random.shuffle(tf.range(start=0, limit=data_sdf[1].shape[0]))]
 
             start = time.time()
             err, latent = reconstruct(
@@ -268,20 +283,20 @@ if __name__ == "__main__":
 
             logging.debug("latent: {}".format(latent.detach().cpu().numpy()))
 
-            decoder.eval()
+            # decoder.eval()
 
             if not os.path.exists(os.path.dirname(mesh_filename)):
                 os.makedirs(os.path.dirname(mesh_filename))
 
             if not save_latvec_only:
                 start = time.time()
-                with torch.no_grad():
-                    deep_sdf.mesh.create_mesh(
-                        decoder, latent, mesh_filename, N=256, max_batch=int(2 ** 18)
-                    )
+                deep_sdf.mesh.create_mesh(
+                    decoder, latent, mesh_filename, N=256, max_batch=int(2 ** 18)
+                )
                 logging.debug("total time: {}".format(time.time() - start))
 
             if not os.path.exists(os.path.dirname(latent_filename)):
                 os.makedirs(os.path.dirname(latent_filename))
 
-            torch.save(latent.unsqueeze(0), latent_filename)
+            # TODO:
+            torch.save(tf.expand_dims(latent, axis=0), latent_filename)
