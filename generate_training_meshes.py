f1a765f7b636c8b3e7593f7052c28ece023d2aed
dkasuga <benjamin.at.mac@gmail.com>
2020-08-30 17:27:56 +0900

    change from torch.save to tf.train.Checkpoint.save

--- generate_training_meshes.py
+++ generate_training_meshes.py
@@ -5,7 +5,7 @@ import argparse
 import json
 import numpy as np
 import os
-import torch
+import tensorflow as tf
 
 import deep_sdf
 import deep_sdf.workspace as ws
@@ -13,133 +13,123 @@ import deep_sdf.workspace as ws
 
 def code_to_mesh(experiment_directory, checkpoint, keep_normalized=False):
 
-    specs_filename = os.path.join(experiment_directory, "specs.json")
+  specs_filename = os.path.join(experiment_directory, "specs.json")
 
-    if not os.path.isfile(specs_filename):
-        raise Exception(
-            'The experiment directory does not include specifications file "specs.json"'
-        )
-
-    specs = json.load(open(specs_filename))
-
-    arch = __import__("networks." + specs["NetworkArch"], fromlist=["Decoder"])
-
-    latent_size = specs["CodeLength"]
-
-    decoder = arch.Decoder(latent_size, **specs["NetworkSpecs"])
-
-    # TODO:
-    decoder = torch.nn.DataParallel(decoder)
-
-    # TODO:
-    saved_model_state = torch.load(
-        os.path.join(experiment_directory,
-                     ws.model_params_subdir, checkpoint + ".ckpt")
+  if not os.path.isfile(specs_filename):
+    raise Exception(
+        'The experiment directory does not include specifications file "specs.json"'
     )
-    saved_model_epoch = saved_model_state["epoch"]
 
-    # TODO:
-    decoder.load_state_dict(saved_model_state["model_state_dict"])
+  specs = json.load(open(specs_filename))
 
-    # decoder = decoder.module.cuda()
+  arch = __import__("networks." + specs["NetworkArch"], fromlist=["Decoder"])
 
-    # decoder.eval()
+  latent_size = specs["CodeLength"]
 
-    latent_vectors = ws.load_latent_vectors(experiment_directory, checkpoint)
+  decoder = arch.Decoder(latent_size, **specs["NetworkSpecs"])
 
-    train_split_file = specs["TrainSplit"]
+  model_filename = os.path.join(experiment_directory,
+                                ws.model_params_subdir, checkpoint + ".ckpt")
+  ckpt = tf.train.Checkpoint(
+      decoder=decoder, epoch=tf.Variable(0, dtype=tf.int64))
+  ckpt.restore(model_filename)
+  saved_model_epoch = ckpt.epoch
 
-    with open(train_split_file, "r") as f:
-        train_split = json.load(f)
+  latent_vectors = ws.load_latent_vectors(experiment_directory, checkpoint)
 
-    data_source = specs["DataSource"]
+  train_split_file = specs["TrainSplit"]
 
-    instance_filenames = deep_sdf.data.get_instance_filenames(
-        data_source, train_split)
+  with open(train_split_file, "r") as f:
+    train_split = json.load(f)
 
-    print(len(instance_filenames), " vs ", len(latent_vectors))
+  data_source = specs["DataSource"]
 
-    for i, latent_vector in enumerate(latent_vectors):
+  instance_filenames = deep_sdf.data.get_instance_filenames(
+      data_source, train_split)
 
-        dataset_name, class_name, instance_name = instance_filenames[i].split(
-            "/")
-        instance_name = instance_name.split(".")[0]
+  print(len(instance_filenames), " vs ", len(latent_vectors))
 
-        print("{} {} {}".format(dataset_name, class_name, instance_name))
+  for i, latent_vector in enumerate(latent_vectors):
 
-        mesh_dir = os.path.join(
-            experiment_directory,
-            ws.training_meshes_subdir,
-            str(saved_model_epoch),
-            dataset_name,
-            class_name,
-        )
-        print(mesh_dir)
+    dataset_name, class_name, instance_name = instance_filenames[i].split(
+        "/")
+    instance_name = instance_name.split(".")[0]
 
-        if not os.path.isdir(mesh_dir):
-            os.makedirs(mesh_dir)
+    print("{} {} {}".format(dataset_name, class_name, instance_name))
 
-        mesh_filename = os.path.join(mesh_dir, instance_name)
+    mesh_dir = os.path.join(
+        experiment_directory,
+        ws.training_meshes_subdir,
+        str(saved_model_epoch),
+        dataset_name,
+        class_name,
+    )
+    print(mesh_dir)
 
-        print(instance_filenames[i])
+    if not os.path.isdir(mesh_dir):
+      os.makedirs(mesh_dir)
 
-        offset = None
-        scale = None
+    mesh_filename = os.path.join(mesh_dir, instance_name)
 
-        if not keep_normalized:
+    print(instance_filenames[i])
 
-            normalization_params = np.load(
-                ws.get_normalization_params_filename(
-                    data_source, dataset_name, class_name, instance_name
-                )
-            )
-            offset = normalization_params["offset"]
-            scale = normalization_params["scale"]
+    offset = None
+    scale = None
 
-        deep_sdf.mesh.create_mesh(
-            decoder,
-            latent_vector,
-            mesh_filename,
-            N=256,
-            max_batch=int(2 ** 18),
-            offset=offset,
-            scale=scale,
-        )
+    if not keep_normalized:
+
+      normalization_params = np.load(
+          ws.get_normalization_params_filename(
+              data_source, dataset_name, class_name, instance_name
+          )
+      )
+      offset = normalization_params["offset"]
+      scale = normalization_params["scale"]
+
+    deep_sdf.mesh.create_mesh(
+        decoder,
+        latent_vector,
+        mesh_filename,
+        N=256,
+        max_batch=int(2 ** 18),
+        offset=offset,
+        scale=scale,
+    )
 
 
 if __name__ == "__main__":
 
-    arg_parser = argparse.ArgumentParser(
-        description="Use a trained DeepSDF decoder to generate a mesh given a latent code."
-    )
-    arg_parser.add_argument(
-        "--experiment",
-        "-e",
-        dest="experiment_directory",
-        required=True,
-        help="The experiment directory which includes specifications and saved model "
-        + "files to use for reconstruction",
-    )
-    arg_parser.add_argument(
-        "--checkpoint",
-        "-c",
-        dest="checkpoint",
-        default="latest",
-        help="The checkpoint weights to use. This can be a number indicated an epoch "
-        + "or 'latest' for the latest weights (this is the default)",
-    )
-    arg_parser.add_argument(
-        "--keep_normalization",
-        dest="keep_normalized",
-        default=False,
-        action="store_true",
-        help="If set, keep the meshes in the normalized scale.",
-    )
-    deep_sdf.add_common_args(arg_parser)
+  arg_parser = argparse.ArgumentParser(
+      description="Use a trained DeepSDF decoder to generate a mesh given a latent code."
+  )
+  arg_parser.add_argument(
+      "--experiment",
+      "-e",
+      dest="experiment_directory",
+      required=True,
+      help="The experiment directory which includes specifications and saved model "
+      + "files to use for reconstruction",
+  )
+  arg_parser.add_argument(
+      "--checkpoint",
+      "-c",
+      dest="checkpoint",
+      default="latest",
+      help="The checkpoint weights to use. This can be a number indicated an epoch "
+      + "or 'latest' for the latest weights (this is the default)",
+  )
+  arg_parser.add_argument(
+      "--keep_normalization",
+      dest="keep_normalized",
+      default=False,
+      action="store_true",
+      help="If set, keep the meshes in the normalized scale.",
+  )
+  deep_sdf.add_common_args(arg_parser)
 
-    args = arg_parser.parse_args()
+  args = arg_parser.parse_args()
 
-    deep_sdf.configure_logging(args)
+  deep_sdf.configure_logging(args)
 
-    code_to_mesh(args.experiment_directory,
-                 args.checkpoint, args.keep_normalized)
+  code_to_mesh(args.experiment_directory,
+               args.checkpoint, args.keep_normalized)
